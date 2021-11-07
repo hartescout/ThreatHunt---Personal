d85fac999f99918f2df9a12308e83cbed73df16f
hartescout <hartescout@protonmail.com>
2021-11-07 13:29:40 -0800

    added 2

---
+++ random_scripts/extract2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import py7zr, os, lzma
+
+pwd = input("Enter the password. If no password enter 'n/a': ")
+directory = input("\nEnter full path to extract files to: ")
+for file in os.listdir():
+    if file.endswith(".7z"):
+        if py7zr.SevenZipFile.needs_password(True):
+                with py7zr.SevenZipFile(file, 'r', password='pwd') as archive:
+                    try:
+                        print("Files will be extracted to ", directory)
+                        os.mkdir(directory)
+                    except FileExistsError:
+                        directory = input("\nDirectory or file exists, choose another. Enter full path of directory to extract the files: ")
+                        print("Files will be extracted to ", directory)
+                        pass
+                    except FileExistsError:
+                        print("\n\n\nCheck filepath and try again.")
+                        quit()
+                    pass
+for file in os.listdir():
+        if file.endswith(".7z"):
+            with py7zr.SevenZipFile(file, 'r', password=pwd) as archive:
+                archive.extractall(path=directory)
